9615f3bc86229bb88cf018e753a179ccd2f9676e
Seung-Il Park <72644713+bob8dod@users.noreply.github.com>
2021-09-07 21:33:59 +0900

    Gym suit
    Bad Code

--- Greedy/programmers#42862.py
+++ Greedy/programmers#42862.py
@@ -1,25 +1,21 @@
 def solution(n, lost, reserve):
-    lost.sort()
-    reserve.sort()
-    lost = set(lost)
-    reserve = set(reserve)
+    lost = set(sorted(lost))
+    reserve = set(sorted(reserve))
     same = lost & reserve
-    lost-=same
-    reserve-=same
-    lost = list(lost)
-    reserve = list(reserve)
+    lost = list(lost - same)
+    reserve = list(reserve - same)
+
     rem_idx = []
     for i in range(len(lost)):
-        if lost[i]-1 in reserve:
-            idx = reserve.index(lost[i]-1)
+        if lost[i] - 1 in reserve:
+            idx = reserve.index(lost[i] - 1)
             rem_idx.append(i)
             reserve.pop(idx)
-        elif lost[i]+1 in reserve:
-            idx = reserve.index(lost[i]+1)
+        elif lost[i] + 1 in reserve:
+            idx = reserve.index(lost[i] + 1)
             rem_idx.append(i)
             reserve.pop(idx)
 
-
-    answer = n-(len(lost)-len(rem_idx))
+    answer = n - (len(lost) - len(rem_idx))
 
     return answer
