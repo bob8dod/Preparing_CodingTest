9abfff9488e95be3ba4ca6d053c41a7b75485f41
Seung-Il Park <72644713+bob8dod@users.noreply.github.com>
2021-09-11 22:32:06 +0900

    2021-09-11 #3

--- kakao/2021-3.py
+++ kakao/2021-3.py
@@ -34,6 +34,3 @@ def solution(fees, records):
                 added_fee = (t - base_time)//add_time + 1
             answer.append(base_fee + added_fee * add_fee)
     return answer
-
-
-print(solution([180, 5000, 10, 600], ["05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"]))
