1642c7c38a266596bd78b8785877f7d02c749748
Seung-Il Park <72644713+bob8dod@users.noreply.github.com>
2021-11-14 23:10:21 +0900

    Update sum_of.py

--- Assistant/sum_of.py
+++ Assistant/sum_of.py
@@ -1,5 +1,7 @@
 n = int(input())
 s=[]
+
+# append elements in list
 for i in range(n):
     p = int(input())
     s.append(p)
@@ -7,10 +9,11 @@ for i in range(n):
 even_total = 0
 odd_total = 0
 for i in range(n):
-    if s[i] % 2 == 0:
+    if s[i] % 2 == 0: # even
         even_total += s[i]
-    else:
+    else: # odd
         odd_total += s[i]
 
+# result of Code
 print('sum of even :',even_total)
 print('sum of odd :',odd_total)
