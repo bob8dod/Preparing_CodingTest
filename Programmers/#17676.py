2420332b765d7919f1aec849fb769cfac7156fdb
Seung-Il Park <72644713+bob8dod@users.noreply.github.com>
2022-01-13 23:41:54 +0900

    추석 트래픽 (Level 3)

--- Programmers/#17676.py
+++ Programmers/#17676.py
@@ -1,36 +1,47 @@
-# 추석트래픽
-def calc_time(tms, T):
-    T = float(T) - 0.001
-    t,m = map(int, tms.split(':')[:-1])
-    s = float(tms.split(':')[-1])
-    end_time = tms
-    if s < T:
-        if m == 0:
-            t = t -1
-            m += 60
-        m = m-1
-        s+= 60
-
-    s -= T
-
-    start_time = '{}:{:02d}:{:.3f}'.format(t,m,s)
-    return start_time, end_time
-
+# 추석 트래픽_90min
+"""
+먼저 시간을 ms 단위로 전부 바꿔준 후 시작시간과 끝시간을 처리시간 T를 통해 계산하여 check 배열에 저장
+그 후 최대로 많이 처리한 것을 구하기 위해
+각 요소들의 끝시간을 기준으로 +1s 의 구간을 설정하고
+나머지 요소들이 해당 구간안에 들어오는지를 판단.
+이는 4가지 경우로 나뉘어서 해결
+1) 시작시간 끝시간 모두 구간안에 들어오는 경우
+2) 시작시간만 구간안에 들어오는 경우
+3) 끝시간만 구간안에 들어오는 경우
+4) 시작시간과 끝시간의 범위가 구간을 초과하는 경우
+$ 1번 가정은 사실 2,3번가정과 겹치므로 생략
+결로적으로 2,3,4 가정에 해당하는 요소들에 개수를 파악
+"""
 def find(check):
+    max_cnt = 0
     for i in check:
-        check_time = i[1] + 0.999
+        cnt = 0
+        for j in check:
+            if i[1] <= j[0] <= i[1]+999:
+                cnt+=1
+                continue
+            if i[1] <= j[1] <= i[1]+999:
+                cnt+=1
+                continue
+            if j[0] < i[1] and j[1] > i[1]+999:
+                cnt+=1
+                continue
 
+        if cnt > max_cnt: max_cnt = cnt
+    return max_cnt
 
 
 def solution(lines):
-    check = []
+    check=[]
     for li in lines:
-        li_sp = li.split()
-        tms = li_sp[1]
-        T= li_sp[2][:-1]
+        t,m,s = li.split()[1].split(':')
+        t, m = int(t), int(m)
+        s = float(s)
+        T = int((float(li.split()[2][:-1]) - 0.001)*1000)
 
-        s,e = calc_time(tms,T)
-        check.append([s, e])
+        to_ms = int(s*1000 + m*60*1000 + t*60*60*1000)
+        start_time, end_time = to_ms - T , to_ms
+        check.append([start_time, end_time])
 
     answer = find(check)
     return answer
