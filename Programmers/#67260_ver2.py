427b00a579f61734cc57066b4ddce4071f870e17
Seung-Il Park <72644713+bob8dod@users.noreply.github.com>
2022-01-17 00:07:15 +0900

    동굴 탐험 BestCode (Using Queue)

--- Programmers/#67260_ver2.py
+++ Programmers/#67260_ver2.py
@@ -1,57 +1,50 @@
-# 2020카카오 인턴십 _ more than 90min
+# 동굴탐험 BestCode
 """
-결론적으로 효율성 마지막 문제를 해결하지 못함. (재귀로 풀었기 때문이라 생각)
-먼저 그래프를 생성해주고 (양 방향 그래프)
-그 그래프에 대해서 DFS를 통해 serach해줌
-이때, 내가 들른 노드가 leaf 노드인지를 체크하며
-해당 노드 밑으로 갈 필요가 없는지를 반복해서 체크 -> 이걸 leaf노드라고 설정
-한 DFS가 끝날 때마다, 모든 노드를 찾았는지, 찾지 못했다면 leaf노드에 변화가 있는지를 확인하여
-True False 판단단"""
-from collections import defaultdict
-graph = defaultdict(list)
+핵심 개념은 내가 넘어가지 못하는 노드에 대해 접근했을때,
+이 노드를 열어주는 연결 노드에 대해 기록하고 그 연결 노드에 visited했을 때, 바로 못넘었던 노드로 연결해주는 것
+접근하지도 않았는데 연결 노드에 도착했다고 바로 막힌 노드로 넘어가게 되면 놓치는 노드가 있을 수 있음
+그니깐 가장 중요한건 못가는 노드라고 아예 배제하는 것이 아닌 접근기록을 남기는 것
+큐or스택에 넣어준 다음에 연결노드에 방문하면 바로 그 막힌 노드로 넘어가는 것
+"""
 
-dict_order = {}
-stop = {}
-leaf = {}
+from collections import deque
 
-def dfs(i,prev):
-    if i in dict_order:
-        del stop[dict_order[i]]
-        del dict_order[i]
+def bfs(i):
+    q = deque([i])
+    while q:
+        curr = q.popleft()
 
-    if len(graph[i]) == 1:
-        leaf[i] = 1
-        return
+        if orders[curr] and not visited[orders[curr]]:
+            after[orders[curr]] = curr
+            continue
 
-    for j in graph[i]:
-        if j != prev and j not in stop and j not in leaf:
-            dfs(j,i)
+        visited[curr] = True
 
-    cnt = 0
-    for j in graph[i]:
-        if j != prev and j in leaf: cnt+=1
+        for next in tree[curr]:
+            if not visited[next]:
+                q.append(next)
 
-    if i == 0:
-        if len(graph[i]) == cnt:
-            leaf[i] = 1
-    else:
-        if len(graph[i])-1 == cnt:
-            leaf[i] = 1
+        if curr in after:
+            q.append(after[curr])
+
+    return
 
 
 def solution(n, path, order):
+    # path 정보로 부터 인접 리스트를 구현한다.
+    global tree, orders, visited, after
+    tree = [[] for _ in range(n)]
+    orders = [0 for i in range(n)]
+    visited = [False for i in range(n)]
+    after = {}
 
-    for i in path:
-        graph[i[0]].append(i[1])
-        graph[i[1]].append(i[0])
+    for v1, v2 in path:
+        tree[v1].append(v2)
+        tree[v2].append(v1)
 
-    for i in order:
-        dict_order[i[0]] = i[1]
-        stop[i[1]] = True
+    for pre, post in order:
+        orders[post] = pre
 
-    while True:
-        before = leaf.copy()
-        if 0 not in stop:
-            dfs(0,-1)
-        if len(leaf) == n: return True
-        if before == leaf: return False
+    bfs(0)
+
+    return n == len(visited)
