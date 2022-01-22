1bf3c61764f370eec34206fd20e318e4589a3973
Seung-Il Park <72644713+bob8dod@users.noreply.github.com>
2022-01-22 18:52:19 +0900

    Create #1612.py

---
+++ BaekJoon/#1612.py
@@ -0,0 +1,19 @@
+# 가지고 노는 1
+"""
+중요X _ 수학적인 문제
+(i*10+1)%N == ((i%N)*10+1)%N 과 똑같은 것을 통해서
+수를 줄여 연산 부담을 덜해줌. -> 시간 초과 해결
+"""
+def solution(N):
+    if N%2==0 or N%5==0:
+        return -1
+    i = 1
+    cnt = 1
+    while i%N != 0:
+        i = (i%N)*10+1
+        cnt+=1
+
+    return cnt
+
+N = int(input())
+print(solution(N))
