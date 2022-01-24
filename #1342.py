10eca8c78a90e710f3c12f2b5b1fb7bf6c378bc5
Seung-Il Park <72644713+bob8dod@users.noreply.github.com>
2022-01-24 20:27:43 +0900

    Wrong

--- #1342.py
+++ #1342.py
@@ -1,16 +1,36 @@
-#1342 행운의 문자열 WRONG
-from itertools import permutations
+#행운의 문자열
+from collections import Counter
+from math import factorial
+
+def dfs(i, s):
+    global result
+    if i == len(S):
+        result += 1
+        return
+
+    for c in s_dict.keys():
+        if s_dict[c] == 0:
+            continue
+        if len(s)==0:
+            s_dict[c] -= 1
+            dfs(i + 1, c)
+            s_dict[c] += 1
+        elif s != c:
+            # if sum([1 for i in s_dict.items() if i[1] == 1]) == len(s_dict):
+            #     result += factorial()
+            s_dict[c] -= 1
+            dfs(i + 1, c)
+            s_dict[c] += 1
+
+
+def solution(S):
+    global result, s_dict
+    s_dict = Counter(S)
+    result = 0
+    dfs(0,'')
+
+    return result
+
 
 S = input()
-cnt = 0
-print(list(permutations(S, len(S))))
-for tem in permutations(S, len(S)):
-    flag = True
-    for i in range(len(tem)-1):
-        if tem[i] == tem[i+1]:
-            flag = False
-            break
-
-    if flag: cnt+=1
-
-print(cnt)
+print(solution(S))
