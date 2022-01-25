c595b7860b9688396730c6a6958cf3af7d2ce0a0
Seung-Il Park <72644713+bob8dod@users.noreply.github.com>
2022-01-25 15:33:04 +0900

    행운의 문자열 (난이도 中, Siver1)

--- #1342.py
+++ #1342.py
@@ -1,4 +1,15 @@
 #행운의 문자열
+"""
+일단 해당 문자열에서 문자들이 각각 얼마나 나온지 Count로 측정
+결과의 dict를 이용해서 dfs를 통해 가지를 내리며 가능한 경우를 측정
+이때 즉, 가지를 내릴 시 현재 노드를 제외하고 모든 노드가 0 or 1이면
+그냥 거기서 팩토리얼 계산 때려버림
+여기서 중요한 것은 얘가 가지를 내릴지 안내릴지 판단하는 부분에서
+가지를 내린다면 dict에서 해당 문자에 대한 count를 -1 해주는데
+이 dict는 공유되고 있기에 가지를 내리고 돌아올 때 다시 count를 +1 해줌으로써
+그 층에서의 dict를 복원해줘야 함. -> dfs이므로
+"""
+
 from collections import Counter
 from math import factorial
 
@@ -8,6 +19,21 @@ def dfs(i, s):
         result += 1
         return
 
+    if len(s)==0 or s_dict[s] == 0:
+        cnt = 0
+        flag = True
+        for j in s_dict.values():
+            if j == 0:
+                continue
+            if j == 1:
+                cnt += 1
+            else:
+                flag = False
+                break
+        if flag:
+            result += factorial(cnt)
+            return
+
     for c in s_dict.keys():
         if s_dict[c] == 0:
             continue
@@ -16,8 +42,6 @@ def dfs(i, s):
             dfs(i + 1, c)
             s_dict[c] += 1
         elif s != c:
-            # if sum([1 for i in s_dict.items() if i[1] == 1]) == len(s_dict):
-            #     result += factorial()
             s_dict[c] -= 1
             dfs(i + 1, c)
             s_dict[c] += 1
