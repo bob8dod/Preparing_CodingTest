1cbf246ac00805b2e02b82f067e0160d466b3b88
Seung-Il Park <72644713+bob8dod@users.noreply.github.com>
2022-02-06 23:32:38 +0900

    정수 삼각형 BestCode

--- Programmers/DP/#43105_opt.py
+++ Programmers/DP/#43105_opt.py
@@ -3,7 +3,7 @@
 같은 풀이 방법이지만,
 따로 tri_map이라는 것을 만들지 않고
 주어진 trianlge의 인덱스를 그대로 사용해서 푼 버전
-# 결론 : 굳이 tri_map을 만들어서 판단할 필요가 없었다..
+# 결론 : 굳이 tri_map을 만들어서 판단할 필요가 없었다. 주어진 input값을 그대로 사용해보자.
 """
 def solution(triangle):
     for i in range(1, len(triangle)):
