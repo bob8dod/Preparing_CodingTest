3a1ee1880ac92c5f120cf94179ff8151e73d401f
Seung-Il Park <72644713+bob8dod@users.noreply.github.com>
2022-03-06 00:03:10 +0900

    Delete Sort directory

--- Sort/programmers#42746(2).py
+++
@@ -1,5 +0,0 @@
-def solution(numbers):
-    numbers = list(map(str, numbers)) # 문자열로써 비교하기 위해
-    # 가장 긴 수의 길이가 최대 3이므로 (1000보다 클 수 없음) 비교를 위해 해당 수를 3번 복사해주고 문자열로써 비교하여 정렬
-    numbers.sort(key=lambda x: x*3, reverse=True) 
-    return str(int(''.join(numbers))) #정렬 결과를 반환
