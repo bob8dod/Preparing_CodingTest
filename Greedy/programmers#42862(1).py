daf4d2346c41371b074fae70a17376f7b272c94d
Seung-Il Park <72644713+bob8dod@users.noreply.github.com>
2022-03-06 00:03:47 +0900

    Delete Greedy directory

--- Greedy/programmers#42862(1).py
+++
@@ -1,9 +0,0 @@
-def solution(n, lost, reserve):
-    after_reserve = [i for i in reserve if i not in lost]
-    after_lost = [i for i in lost if i not in reserve]
-    for r in after_reserve:
-        if r-1 in after_lost:
-            after_lost.remove(r-1)
-        elif r+1 in after_lost:
-            after_lost.remove(r+1)
-    return n - len(after_lost)
